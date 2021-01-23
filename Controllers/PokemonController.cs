87a040688ae45ac34c824b656322ebf41b6aa771
Atilas Santos da Silva <atilasabc@gmail.com>
2021-01-23 14:05:54 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_de_pokemon.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+    }
+}
